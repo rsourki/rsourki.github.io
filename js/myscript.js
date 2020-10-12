26afaf4421b972050d2c324796e89702a424fb01
rsourki <58962192+rsourki@users.noreply.github.com>
2020-10-12 15:46:05 -0700

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,10 @@
-$(".bar").each(function(){
+/* $(".bar").each(function(){
   $(this).find(".bar-inner").animate({
     width: $(this).attr("data-width")
-  },2000)
+  },2000)*/
 }); 
-/* $(window).on('load', function() {
+$(window).on('load', function() {
   $(".bar-inner").animate({
     width: $(this).attr("data-width")
   },2000)
-});*/
+});
